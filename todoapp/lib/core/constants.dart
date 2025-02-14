107ed0a2074d7def78d9f52fa08debb0822b942d
fatmakhadidja <154927228+fatmakhadidja@users.noreply.github.com>
2025-02-14 22:33:48 +0100

    first commit

---
+++ todoapp/lib/core/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyColors{
+  final Color mainPurple = Color(0xff9747FF);
+  final Color blue = Color(0xffB4C4FF);
+  final Color green = Color(0xffCFF3E9);
+  final Color brown = Color(0xffF4D8B1);
+  final Color grey = Color(0xff959595);
+  final Color red = Color(0xffE90707);
+  final Color lightPurple = Color(0xffE0C8FF);
+}
